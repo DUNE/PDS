7b7b4b0c0e24b9d24cab479d73899bb3b5fe2da9
Anselmo Cervera Villanueva <acervera@lxplus9110.cern.ch>
2024-03-26 17:49:44 +0100

    add tools for converting hdf5 selft trigger fragments to root tree

--- src/pdstools/__init__.py
+++ src/pdstools/__init__.py
@@ -1,7 +1,8 @@
 """
 PDSTools
 
-Provides a means to load DAPHNEStreams
+Provides a means to load DAPHNEStreams and DAPHNE
 from DUNE-DAQ HDF5 files.
 """
 from .DAPHNEStreamData import DAPHNEStreamData
+from .DAPHNEData import DAPHNEData
