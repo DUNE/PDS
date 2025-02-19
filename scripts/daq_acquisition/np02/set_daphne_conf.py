cc5df51df0a1a85984ebe8d042874626ec18c727
Sam Fogarty <samuel.fogarty@colostate.edu>
2025-02-19 16:49:36 +0100

    Add log file functionality, rather than printing to screen

--- scripts/daq_acquisition/np02/set_daphne_conf.py
+++ scripts/daq_acquisition/np02/set_daphne_conf.py
@@ -3,6 +3,12 @@ import click
 import json
 import xml.etree.ElementTree as ET
 import pprint
+import logging
+from datetime import datetime
+
+def RaiseAndLogError(message):
+    logging.error('Error: ' + message)
+    raise RuntimeError(message)
 
 @click.command(help="details_path: path to details file to modify (required) \n \
                      xml_path: path to DAPHNE configuration XML file (required) )\n \
@@ -21,72 +27,93 @@ def cli(details_path, xml_path, obj_name, attenuators, daphne_channels, bias, tr
     """ Script to update DAPHNE configuration files 
         Author: Sam Fogarty
     """
+    details_dir = os.path.dirname(details_path) or '.' # assumes output.json and seed.py in same dir as details.json
+    log_filename = os.path.join(details_dir, 'log', datetime.now().strftime("daphne_config_log_%Y-%m-%d_%H-%M-%S.log"))
+    os.system(f"mkdir -p {os.path.join(details_dir, 'log')}")
+    logging.basicConfig(
+        filename=log_filename,
+        filemode="w",  # Overwrite each time (use "a" for append mode)
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        level=logging.INFO
+    )
+    logging.info(f'details_path = {details_path}')
+    logging.info(f'xml_path = {xml_path}')
+    logging.info(f'Modifying object: {obj_name}')
+    print(f'logs being stored to {log_filename}')
     if not os.path.exists(details_path):
-        raise Exception('details.json does not exist at ', details_path)
+        RaiseAndLogError(f'details.json does not exist at {details_path}')
     if not os.path.exists(xml_path):
-        raise Exception('xml file does not exist at ', xml_path)
+        RaiseAndLogError(f'xml file does not exist at {xml_path}')
     if mode is not None and mode not in [0, 1]:
-        raise Exception('Please specify a correct triggering mode (0 for full streaming, 1 for self-triggering), you provided ', mode)
+        RaiseAndLogError(f'Please specify a correct triggering mode (0 for full streaming, 1 for self-triggering), you provided {mode}')
     if mode == 1 and self_trigger_threshold is None:
-        raise Exception('Since you are using self-triggering, please provide a self_trigger_threshold')
+        RaiseAndLogError('Since you are using self-triggering, please provide a self_trigger_threshold')
     
+    # load details.json which we will modify
     with open(details_path, "r") as file:
-        details_json_data = json.load(file)
+        try:
+            details_json_data = json.load(file)
+        except:
+            RaiseAndLogError('Error opening details.json file.')
     
+    # apply modifications based on user inputs to the configuration json
     if bias:
         new_bias = [int(item.strip()) for item in bias.split(",")]
         if len(new_bias) != 5:
-            raise Exception('Error reading bias list, should be five comma-separated values.')
+            RaiseAndLogError('Error reading bias list, should be five comma-separated values.')
         details_json_data["devices"][0]["channels"]["bias"] = new_bias
-
     if attenuators:
         new_attenuators = [int(item.strip()) for item in attenuators.split(",")]
         if len(new_attenuators) != 5:
-            raise Exception('Error reading attenuators list, should be five comma-separated values.')
+            RaiseAndLogError('Problem reading attenuators list, should be five comma-separated values.')
         details_json_data["devices"][0]["channels"]["attenuators"] = new_attenuators
+        logging.info(f'Setting VGAIN values to {new_attenuators}')
     if daphne_channels:
         new_daphne_channels = [int(item.strip()) for item in daphne_channels.split(",")]
         if len(new_daphne_channels) == 0:
-            raise Exception('Error: Must provide a non-empty list of channels.')
+            RaiseAndLogError('Error: Must provide a non-empty list of channels.')
         details_json_data["devices"][0]["channels"]["indices"] = new_daphne_channels
-
+        logging.info(f'Setting channels list to {new_daphne_channels}')
     if trim:
         new_trim = [int(item.strip()) for item in trim.split(",")]
         if len(new_trim) != 40:
-            raise Exception('Error: list of trims must have a length of 40.')
+            RaiseAndLogError('Error: list of trims must have a length of 40.')
         details_json_data["devices"][0]["channels"]["trim"] = new_trim
-    
+        logging.info(f'Setting trims list to {new_trim}')
     if mode is not None:
         if mode == 0:
             details_json_data["devices"][0]["mode"] = "full_streaming"
             details_json_data["devices"][0]["self_trigger_threshold"] = 0
+            logging.info('Setting mode to full streaming')
         else:
             details_json_data["devices"][0]["mode"] = "self-trigger"
             details_json_data["devices"][0]["self_trigger_threshold"] = self_trigger_threshold
-    
+            logging.info(f'Setting mode to self-trigger, with threshold of {self_trigger_threshold}')
     if offsets:
         new_offsets = [int(item.strip()) for item in offsets.split(",")]
         if len(new_offsets) != 40:
-            raise Exception('Error: list of offsets must have a length of 40.')
+            RaiseAndLogError('Error: list of offsets must have a length of 40.')
         details_json_data["devices"][0]["channels"]["offsets"] = new_offsets
-
-    with open(details_path, "w") as file:
+        logging.info(f'Setting offsets to {new_offsets}')
+    with open(details_path, "w") as file: # replace details file with updated one
         json.dump(details_json_data, file, indent=4)
     
-    details_dir = os.path.dirname(details_path) or '.'
     output_path = os.path.join(details_dir, 'output.json')
     seed_path = os.path.join(details_dir, 'seed.py')
     
+    # overwrite output.json with info from details.json
     command = f'python3 {seed_path} --details {details_path} --output {output_path}'
-    print('seed command: ', command)
+    logging.info(f'seed command: {command}')
     os.system(command)
     
+    # update object with new DAPHNE config
     command = f'add_daphne_conf {xml_path} {output_path} -n {obj_name}'
-    print(command)
+    logging.info(f'add daphne conf command: {command}')
     os.system(command)
     
+    # print configuration as it is inside XML file
     root = ET.parse(xml_path)
-    daphne_conf = root.find(".//obj[@class='DaphneConf'][@id='np02-daphne-running']")
+    daphne_conf = root.find(f".//obj[@class='DaphneConf'][@id='{obj_name}']")
     
     if daphne_conf is not None:
         attributes = {}
@@ -100,12 +127,11 @@ def cli(details_path, xml_path, obj_name, attenuators, daphne_channels, bias, tr
         if key == 'json_file':
             json_file = json.loads(value['value'])
             for key in json_file.keys():
-                print('For key=', key, ', json=')#, json_file[key])
-                pprint.pp(json_file[key])
+                logging.info(f'In the DAPHNE config XML, for key {key}, the stored json = {json_file[key]}')
     
     with open(details_path, "r") as file:
         updated_data = json.load(file)
-        print('Updated DAPHNE config')
+        logging.info('Finished updating DAPHNE config')
 
 if __name__ == '__main__':
     cli()
