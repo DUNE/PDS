9acb5aa654f4ee05c420f7442fdea088495ddc74
np04daq <np04daq@np04-srv-024.cern.ch>
2026-07-28 16:57:37 +0200

    Added option to display fake trigger

--- src/pds/core/butler.py
+++ src/pds/core/butler.py
@@ -21,9 +21,17 @@ class DTSButler:
 
     def run(self, *, hztrigger: Optional[float] = None) -> None:
         align_command = ["bash", "-c", f"cd {self.workdir} && {self.align_cmd}"]
+        if hztrigger is not None and self.fake_cmd_tpl.strip():
+            cmd = self.fake_cmd_tpl.format(hztrigger=hztrigger)
+            fake_trigger_cmd = ["bash", "-c", f"cd {self.workdir} && {cmd}"]
+        else:
+            fake_trigger_cmd = ""
+
         if self.skip:
             _LOG.info("  Skipping Butler alignment (skip_dts=True)...")
             _LOG.info(f"  Aligment command: {' '.join(align_command)}")
+            if fake_trigger_cmd:
+                _LOG.info(f"  If fake-trigger active: {' '.join(fake_trigger_cmd)}")
             return
 
         if self.mode in ("cosmics", "thrscan", "threshold", "sthscan", "selftrigger"):
@@ -36,9 +44,8 @@ class DTSButler:
             subprocess.run(align_command, check=True)
 
 
-        if hztrigger is not None and self.fake_cmd_tpl.strip():
-            cmd = self.fake_cmd_tpl.format(hztrigger=hztrigger)
-            subprocess.run(["bash", "-c", f"cd {self.workdir} && {cmd}"], check=True)
+        if fake_trigger_cmd:
+            subprocess.run(fake_trigger_cmd, check=True)
             _LOG.info("✅  DTS fake-trigger configured.")
 
     def clear(self) -> None:
